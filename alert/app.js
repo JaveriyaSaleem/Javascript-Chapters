c85a5c4bf9ec64cff5e52443eeeb4152248b6d2f
JaveriyaSaleem <javeriyasaleem2@gmail.com>
2024-04-21 13:07:00 +0500

    alert chapter

---
+++ alert/app.js
@@ -0,0 +1,10 @@
+alert("Error! Please enter a valid password.");
+alert("Welcome to JS Land...\n Happy Coding!");
+alert("Welcome to JS Land..");
+alert("Happy Coding! \n Prevent this page from creating addional dialogs.");
+
+
+var alertcons = "Hello.. I can run JS through my web browsers console"
+console.log (alertcons);
+
+alert(alertcons);
