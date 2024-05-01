1ddeab17caefe9cf4e4c63ebb30bc841e332cf96
JaveriyaSaleem <javeriyasaleem2@gmail.com>
2024-05-02 00:06:00 +0500

    chap5 completed!

---
+++ Chap1 Alert/app.js
@@ -0,0 +1,65 @@
+// Chap 1 Alert
+alert("Error! Please enter a valid password.");
+alert("Welcome to JS Land...\n Happy Coding!");
+alert("Welcome to JS Land..");
+alert("Happy Coding! \n Prevent this page from creating addional dialogs.");
+
+// chap2 Variables for Strings 
+var alertcons = "Hello.. I can run JS through my web browsers console"
+console.log (alertcons);
+
+alert(alertcons);
+// Updating var 
+alertcons = "Hello.. See I just updated my var"
+console.log(alertcons);
+
+// Chap 3 number 
+var num = 2;
+var myNum = num + 3;
+console.log(myNum+5);
+
+// Chap 4 Variable Names Legal and Illegal 
+
+// Declare a variable using nothing but all of the legal characters that aren't alphabet characters or numbers. Don't repeat any of them. (The timer starts when you click in the answer field.)
+// var $_;
+
+// In a single statement declare a legally-named variable and assign a number to it.
+// var $_ = 2;
+// In a single statement declare a legally-named variable and assign to it the sum of 2 other legally-named variables.
+// var myNumber = add + sum;
+
+// Declare a variable whose name is your first and last names combined, in camelCase.
+// Assign the variable your first and last names, as a string.
+// Code an alert, specifying the variable, not the string, as the message.
+// Click the Run It button to run your code live.
+
+// var javeriyaSaleem;
+// javeriyaSaleem = "Heya!!";
+// alert(javeriyaSaleem);
+
+// Declare a variable that combines a number, an alphabet letter, and one of the legal characters that are neither an alphabet letter nor a number.
+// Assign to the variable a string consisting of another legal character that is neither an alphabet letter nor a number.
+// Display the variable's value in an alert, specifying the variable, not the value, as the message.
+// var my1$;
+// my1$ = "**";
+// alert(my1$);
+
+// chapter 5 Math expressions: Familiar operators
+// modulus gives you remaining number in division 
+var myMod = 9  % 4;
+console.log(myMod);
+// answer was 1 
+// Assign to a variable the remainder when the value represented by a variable is divided by a number. The first variable has been declared beforehand. Make up the variable names.
+// modMy = add % 2;
+
+// Code an alert that displays the result of a math operation on 2 numbers.
+alert(80 % 18); 
+
+// Assign numbers to 2 new variables. Code an alert that displays the result of a math operation on the variables.
+var myNums = 2;
+var addNew = 3;
+alert(myNums + addNew);
+
+
+
+
