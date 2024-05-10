df3f6b1ddbcb72e228a8973246baffc153d5a0c9
JaveriyaSaleem <javeriyasaleem2@gmail.com>
2024-05-10 19:44:27 +0500

    added question

--- 10-11/app.js
+++ 10-11/app.js
@@ -74,6 +74,75 @@ if ("car" < "cat") {
 // subjects & total marks. Compute & show the resulting
 // percentage on your page. Take percentage & compute
 // grade as per following table:
+document.write("<h2>Question 06</h2>")
+var totalMarks = 100;
+var subject1 = prompt("Enter Your 1st Subject Name here");
+var subject1Marks = prompt("Enter Your 1st Subject Marks here");
+var subject2 = prompt("Enter Your 2nd Subject Name here");
+var subject2Marks = prompt("Enter Your 2nd Subject Marks here");
+var subject3 = prompt("Enter Your 3rd Subject Name here");  
+var subject3Marks = prompt("Enter Your 3rd Subject Marks here");
+var resultFormula1 = subject1Marks/totalMarks*100;
+var resultFormula2 = subject2Marks/totalMarks*100;
+var resultFormula3 = subject2Marks/totalMarks*100;
+var finalMarksObt = +subject1Marks + +subject2Marks + +subject3Marks;
+var finalTotal = 300;
+var percentageFinal =  finalMarksObt/finalTotal*100;
 
+document.write ( 
+  // Start of table with paddings
+  "<table cellpadding='5' cellspacing='0'>" +
+  
+  // Header
+  "<thead><tr><th><b>Subject</b></th><th><b>Total Marks</b></th><th><b>Marks Obtained</b></th><th><b>Percentage</b></th></tr></thead>" +
+  
+  // Body
+  "<tbody>" +
+      "<tr><td>" + subject1 + "</td><td>" + totalMarks + "</td><td>" + subject1Marks + "</td><td>" + resultFormula1 + "%</td></tr>" +
+      "<tr><td>" + subject2 + "</td><td>" + totalMarks + "</td><td>" + subject2Marks + "</td><td>" + resultFormula2 + "%</td></tr>" +
+      "<tr><td>" + subject3 + "</td><td>" + totalMarks + "</td><td>" + subject3Marks + "</td><td>" + resultFormula3 + "%</td></tr>" +
+  "</tbody>" +
+
+  // Footer
+  "<tfoot><tr><td><b> </b></td>"+ "<td><b>"+ finalTotal +"</b></td>" + "<td><b>" + finalMarksObt + "</b></td>" + "<td><b>" + percentageFinal +"%</b></td>" + "</tr></tfoot>" +
+
+  // End of table
+  "</table>"
+
+
+ );
+if (percentageFinal > 79) {
+  alert("MARKSHEET\n\nTotal marks: " + finalTotal + "\n" +
+    "Marks Obtained: " + finalMarksObt + "\n" +
+    "Percentage: " + percentageFinal + "\n" +
+    "Grade: A+\n" +
+    "Remarks: Excellent!"
+  );
+};
+if (percentageFinal > 69) {
+  alert("MARKSHEET\n\nTotal marks: " + finalTotal + "\n" +
+    "Marks Obtained: " + finalMarksObt + "\n" +
+    "Percentage: " + percentageFinal + "\n" +
+    "Grade: A\n" +
+    "Remarks: Good!"
+  );
+};
+if (percentageFinal > 59) {
+  alert("MARKSHEET\n\nTotal marks: " + finalTotal + "\n" +
+    "Marks Obtained: " + finalMarksObt + "\n" +
+    "Percentage: " + percentageFinal + "\n" +
+    "Grade: B\n" +
+    "Remarks: You need to improve!"
+  );
+};
+if (percentageFinal < 59) {
+  alert("MARKSHEET\n\nTotal marks: " + finalTotal + "\n" +
+    "Marks Obtained: " + finalMarksObt + "\n" +
+    "Percentage: " + percentageFinal + "\n" +
+    "Grade: Fail\n" +
+    "Remarks: Sorry"
+  );
+};
+// end 
 
 
