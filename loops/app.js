f36611566ca4de3f71cb5010bd3fecc68bf36a89
JaveriyaSaleem <javeriyasaleem2@gmail.com>
2024-06-03 18:23:13 +0500

    added

--- loops/app.js
+++ loops/app.js
@@ -1,16 +1,24 @@
 // ======================Loops========================
+// 1. Declare and initialize an empty multidimensional array.
+// (Array of arrays)
+// var aMultidimensional = [[]];
+// 2. Declare and initialize a multidimensional array
+// representing the following matrix:
+aMultidimensional = [[0,1,2,3],[1,0,1,2],[2,1,0,1]];
+document.write(aMultidimensional+"<br>");
+
 
 // 4. Write a program to print multiplication table of any
 // number using for loop. Table number & length should be
 // taken as an input from user.
-var table = +prompt("Enter any number to get the table of it");
-var howManyTimes = +prompt("How many time you want it to multiply");
-document.write(
-    "<h1> Table of "+table+"</h1>"+"<br>")
-for(var i = 1; i<=howManyTimes ;i++){
-    document.write(
-        table+ " x " + i + " = " + (table*i) +"<br>")
-};
+// var table = +prompt("Enter any number to get the table of it");
+// var howManyTimes = +prompt("How many time you want it to multiply");
+// document.write(
+//     "<h1> Table of "+table+"</h1>"+"<br>")
+// for(var i = 1; i<=howManyTimes ;i++){
+//     document.write(
+//         table+ " x " + i + " = " + (table*i) +"<br>")
+// };
 // 6. Generate the following series in your browser. See
 // example output.
 // a. Counting: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
@@ -19,45 +27,45 @@ for(var i = 1; i<=howManyTimes ;i++){
 // d. Odd: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19
 // e. Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
 // answer a 
-document.write(
-    "<h1> Counting"+"</h1>"+"<br>")
-for(var i = 1;i<=15; i++){
-    document.write(
-         i +"," 
-    )
-};
+// document.write(
+//     "<h1> Counting"+"</h1>"+"<br>")
+// for(var i = 1;i<=15; i++){
+//     document.write(
+//          i +"," 
+//     )
+// };
 // answer b 
-document.write(
-    "<h1> Reverse Counting"+"</h1>"+"<br>")
-for(var i = 10;i>=1; i--){
-    document.write(
-         i +"," 
-    )
-};
+// document.write(
+//     "<h1> Reverse Counting"+"</h1>"+"<br>")
+// for(var i = 10;i>=1; i--){
+//     document.write(
+//          i +"," 
+//     )
+// };
 // answer c
-document.write(
-    "<h1> Even"+"</h1>"+"<br>")
-for(var i = 0;i<=10; i++){ //the condition means the times value will be loop 
-    document.write(
-         i*2 +"," 
-    )
-};
+// document.write(
+//     "<h1> Even"+"</h1>"+"<br>")
+// for(var i = 0;i<=10; i++){ //the condition means the times value will be loop 
+//     document.write(
+//          i*2 +"," 
+//     )
+// };
 // answer d
-document.write(
-    "<h1> Odd"+"</h1>"+"<br>")
-for(var i = 0;i<=9; i++){ //the condition means the times value will be loop 
-    document.write(
-         (i*2)+1 +"," 
-    )
-};
+// document.write(
+//     "<h1> Odd"+"</h1>"+"<br>")
+// for(var i = 0;i<=9; i++){ //the condition means the times value will be loop 
+//     document.write(
+//          (i*2)+1 +"," 
+//     )
+// };
 // answer e
-document.write(
-    "<h1> Series"+"</h1>"+"<br>")
-for(var i = 1;i<=10; i++){ //the condition means the times value will be loop 
-    document.write(
-         i*2 +"k," 
-    )
-};
+// document.write(
+//     "<h1> Series"+"</h1>"+"<br>")
+// for(var i = 1;i<=10; i++){ //the condition means the times value will be loop 
+//     document.write(
+//          i*2 +"k," 
+//     )
+// };
 
 
 
