a504459fca399279297d8494635e1ba49e4c48b9
JaveriyaSaleem <javeriyasaleem2@gmail.com>
2024-06-08 21:16:02 +0500

    added

--- My Practice/app.js
+++ My Practice/app.js
@@ -29,4 +29,14 @@ for (var i = 10; i>=0; i--){
 //     }
 //     document.write("<br>");
 //   }
+// counting 
+for (var i = 1; i <= 100; i= i+10){
+for (var j = i; j < i+10; j++){
+    
+     document.write(j+" ");
+      }
+    document.write("<br>");
+
+}
+
  
