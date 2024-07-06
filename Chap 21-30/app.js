f520731c9288b1ca29eecd5e00f97d52aad70511
JaveriyaSaleem <javeriyasaleem2@gmail.com>
2024-07-07 01:05:37 +0500

    added

--- Chap 21-30/app.js
+++ Chap 21-30/app.js
@@ -208,7 +208,13 @@
 // )
 // ============  till chap 21 -25 end  ===========
 // =================== FROM CHAP 26 TO 30 =====================
-
+//                               MATH METHODS 
+// 1. Write a program that takes a positive integer from user &
+// display the following in your browser.
+// a. number
+// b. round off value of the number
+// c. floor value of the number
+// d. ceil value of the number
 
 
 
