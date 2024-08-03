e6cbb789a288fb832815cd6b27eee284cec92969
JaveriyaSaleem <javeriyasaleem2@gmail.com>
2024-08-03 16:22:56 +0500

    added

--- 43-48/app.js
+++ 43-48/app.js
@@ -3,3 +3,19 @@
 function phonePurchased(){
     alert("Thanks for purchasing!!")
 }
+
+function remove(e){
+    e.parentNode.parentNode.remove()
+}
+var counter = 0;
+
+function increase(){
+        document.getElementById("counter").innerText=counter++
+}
+function decrease(){
+    if(counter<0){
+        counter = 0
+    }
+    document.getElementById("counter").innerText = counter--
+    
+}
